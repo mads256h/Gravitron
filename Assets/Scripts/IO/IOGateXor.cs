da0d9e7e619148fc647108f608ddc341508a213e
Mads Mogensen <mads256c@elev.aatg.dk>
2018-10-23 20:16:13 +0200

    Added xor and updated sprite

---
+++ Assets/Scripts/IO/IOGateXor.cs
@@ -0,0 +1,52 @@
+﻿using JetBrains.Annotations;
+
+namespace IO
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+    public sealed class IOGateXor : IOGateBase
+    {
+        [CanBeNull] [UsedImplicitly(ImplicitUseKindFlags.Assign)] public IOGateChild Child1;
+        [CanBeNull] [UsedImplicitly(ImplicitUseKindFlags.Assign)] public IOGateChild Child2;
+
+        private bool _1Enabled = false;
+        private bool _2Enabled = false;
+
+        protected override void CheckInput()
+        {
+            if (OutputEnabled)
+            {
+                if (_1Enabled == _2Enabled)
+                {
+                    OutputDisable();
+                }
+            }
+            else
+            {
+                if (_1Enabled != _2Enabled)
+                {
+                    OutputEnable();
+                }
+            }
+        }
+
+        public override void ChildInputEnable(IOGateChild child)
+        {
+            if (child == Child1)
+                _1Enabled = true;
+            else
+                _2Enabled = true;
+
+            base.ChildInputEnable(child);
+        }
+
+        public override void ChildInputDisable(IOGateChild child)
+        {
+            if (child == Child1)
+                _1Enabled = false;
+            else
+                _2Enabled = false;
+
+            base.ChildInputDisable(child);
+        }
+    }
+}
